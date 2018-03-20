5770b9b07de797e08df5457852cfbe59ee2a147f
Christian Grobmeier <cg@grobmeier.de>
2018-03-20 14:18:59 +0100

    initial commit

---
+++ src/main/java/solutions/grobmeier/caf/App.java
@@ -0,0 +1,13 @@
+package solutions.grobmeier.caf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
